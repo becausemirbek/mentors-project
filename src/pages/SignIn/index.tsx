31c118b1ab95f2668ed86bd16878553901bb49e0
becausemirbek <becausemirbek@gmail.com>
2023-10-03 18:40:49 +0600

    sign-in layout

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,24 @@
+import { Button, OutlinedInput } from "@mui/material";
 import React from "react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <h2>Sign in</h2>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "50%",
+          margin: "0 auto",
+        }}
+      >
+        <OutlinedInput type="text" />
+        <OutlinedInput type="password" />
+        <Button variant="outlined">Submit</Button>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
